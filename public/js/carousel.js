10ccbb98f526cfe76c449ee0fba5f7af5a93551b
Henryc45 <104082238+Henryc45@users.noreply.github.com>
2023-05-05 15:55:26 -0400

    Add files via upload

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,1 +1,23 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+// var counter = 1;
+// setInterval(function(){
+//   document.getElementById('radio' + counter).checked = true;
+//   counter++;
+//   if(counter > 4){
+//     counter = 1;
+//   }
+// }, 5000);
